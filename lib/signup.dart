c7487f87a284fb234f3dcb2cb6e86ca792cff408
verma <malpotra8082@gmail.com>
2025-02-25 19:35:40 +0530

    added signup

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
 
-/*void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Signup Page',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const SignupPage(),
-    );
-  }
-}*/
-
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
 
